090b6bc90fe7324746eeaadbbd1d9bdb594b9a4b
Richard <richard.mzrosa@gmail.com>
2024-02-08 12:12:45 -0300

    [add] new rom case tests

--- mem/ROM_teste_2.vhd
+++ mem/ROM_teste_2.vhd
@@ -14,28 +14,33 @@ BEGIN
 	--  saida <=    "DADO_MEM" WHEN "ADDR_MEM",
 		saida <= 	
 					-- PROGRAMA: compara posições 0x80 e 0x82. Carrega 1 no AC se são iguais e 0 c.c.
-					x"20" when x"00",     -- LDA
-					x"80" when x"01",     -- 0x80
-					x"60" when x"02",     -- NOT
-					x"30" when x"03",     -- ADD
-					x"81" when x"04",     -- 0x81
-					x"30" when x"05",     -- ADD
-					x"82" when x"06",     -- 0x82
-					x"A0" when x"07",     -- JZ
-					x"0D" when x"08",     -- 0x0D
-					x"20" when x"09",     -- LDA
-					x"83" when x"0A",     -- 0x83
-					x"80" when x"0B",     -- JMP
-					x"0F" when x"0C",     -- 0x0F
-					x"20" when x"0D",     -- LDA
-					x"84" when x"0E",     -- 0x84
-					x"F0" when x"0F",     -- HLT
+					x"20" when x"00",    -- LDA
+					x"80" when x"01",    -- 0x80 : A
+					x"60" when x"02",    -- NOT : F5
+					x"30" when x"03",    -- ADD
+					x"81" when x"04",    -- 0x81 : 1+F5 = F6
+					x"60" when x"05",    -- NOT : 09
+					x"50" when x"06",  	-- AND 
+					x"0F" when x"0F",	   -- 0x0F : 09
+					x"30" when x"08", 	-- ADD 
+					x"80" when x"09",	   -- 0x80 : 09+0A = 13
+					x"90" when x"0A", 	-- JN 
+					x"0E" when x"0B", 	-- 0x0E : N load 0
+					x"A0" when x"0C",    -- JZ 
+					x"12" when x"0D",    -- 0x12 : Z load 1
+					x"20" when x"0E",    -- LDA
+					x"83" when x"0F",    -- 0x83 : load 0
+					x"80" when x"10",    -- JMP : jmp hlt
+					x"0F" when x"11",    -- 0x14
+					x"20" when x"12",    -- LDA
+					x"84" when x"13",    -- 0x84 : load 1
+					x"F0" when x"14",    -- HLT
 					-- DADOS  --------------
 					x"0A" when x"80",	   -- 0x0A = 10
 					x"01" when x"81",	   -- 0x01 = 1
 					x"0A" when x"82",	   -- 0x0A = 10
-					x"00" when x"83",	   -- 0x00 = 0 (not equal)
-					x"01" when x"84",	   -- 0x01 = 1 (equal)
+					x"00" when x"83",	   -- 0x00 = 0 (negative)
+					x"01" when x"84",	   -- 0x01 = 1 (zero)
 
 					"00000000" WHEN OTHERS;
 END arch; 
